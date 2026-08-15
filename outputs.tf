4232dd439a16b5c5c217683604d2e794bd345ffb
Daniel Allen <dan@aeternagroup.net>
2026-08-15 01:43:39 +0100

    Update to schema version v6.58.0

--- outputs.tf
+++ outputs.tf
@@ -8,7 +8,7 @@ output "connect_bot_associations_instance_id" {
 }
 output "connect_bot_associations_lex_bot" {
   description = "Map of lex_bot values across all connect_bot_associations, keyed the same as var.connect_bot_associations"
-  value       = { for k, v in aws_connect_bot_association.connect_bot_associations : k => v.lex_bot if v.lex_bot != null && length(v.lex_bot) > 0 }
+  value       = { for k, v in aws_connect_bot_association.connect_bot_associations : k => one(v.lex_bot) if v.lex_bot != null && length(v.lex_bot) > 0 }
 }
 output "connect_bot_associations_region" {
   description = "Map of region values across all connect_bot_associations, keyed the same as var.connect_bot_associations"
